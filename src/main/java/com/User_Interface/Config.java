9befd2b773a0a7e4e38bd41308b446e52d42dc10
T26x0 <panovd379@gmail.com>
2023-11-27 21:13:40 +0500

    fix

--- src/main/java/com/User_Interface/Config.java
+++ src/main/java/com/User_Interface/Config.java
@@ -1,23 +1,8 @@
-package com.ClientSide.Interface;
+package com.User_Interface;
 
 
 import java.util.HashMap;
 import java.util.Map;
-
-/**
- * <h2>Class for visualization client actions</h2>
- * Size:
- * <pre>x = 30</pre>
- * <pre>y = 30</pre>
- */
-public class User_Interface extends Config {
-    public static void main(String[] args) {
-        User_Interface app = new User_Interface();
-//        app.generalRendering();
-//        System.out.println(Arrays.deepToString(app.frameLocation_x));
-    }
-}
-
 
 abstract class Config {
 
@@ -128,3 +113,4 @@ abstract class Config {
         }
     }
 }
+
---
+++ src/main/java/com/User_Interface/User_Interface.java
@@ -0,0 +1,17 @@
+package com.User_Interface;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <h2>Class for visualization client actions</h2>
+ * Size:
+ * <pre>x = 30</pre>
+ * <pre>y = 30</pre>
+ */
+public class User_Interface extends Config {
+    public static void main(String[] args) {
+        User_Interface app = new User_Interface();
+    }
+}
